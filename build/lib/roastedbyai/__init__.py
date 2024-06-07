778081a270cefaa9d0f62d721478b22c7cc328b4
Jvh <jvh.discord@gmail.com>
2024-06-07 22:22:36 +0200

    Delete build directory

--- build/lib/roastedbyai/__init__.py
+++
@@ -1,7 +0,0 @@
-# Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License (jvherck on GitHub)
-
-from .roasted import *
-from .errors import *
-from .models import *
-
-__version__ = "1.2.0"
